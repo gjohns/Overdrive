e72909ebc1ab3608a2a91a51dc710af5c3ac78e3
gjohns <gary.johns@mavs.uta.edu>
2013-10-10 16:17:34 -0500

    revert

---
+++ workspace/AutoPerformanceAnalyzer/src/com/example/autoperformanceanalyzer/Run.java
@@ -0,0 +1,45 @@
+package com.example.autoperformanceanalyzer;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
+import android.view.View;
+
+public class Run extends Activity {
+
+	 @SuppressLint("NewApi")
+	    @Override
+	    protected void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.activity_run);
+
+	        // Make sure we're running on Honeycomb or higher to use ActionBar APIs
+	        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+	            // Show the Up button in the action bar.
+	            getActionBar().setDisplayHomeAsUpEnabled(true);
+	        }
+	    }
+
+	    @Override
+	    public boolean onOptionsItemSelected(MenuItem item) {
+	        switch (item.getItemId()) {
+	        case android.R.id.home:
+	            NavUtils.navigateUpFromSameTask(this);
+	            return true;
+	        }
+	        return super.onOptionsItemSelected(item);
+	    }
+	    public void returnDisplay(View view) {
+	        // Do something in response to button
+	    	Intent intent = new Intent(this, DisplayModeActivity.class);
+	    	//EditText editText = (EditText) findViewById(R.id.History);
+	    	//String message = editText.getText().toString();
+	    	//intent.putExtra(EXTRA_MESSAGE, message);
+	    	startActivity(intent);
+	    }
+
+}
